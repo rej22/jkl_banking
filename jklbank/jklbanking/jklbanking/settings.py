a76fbf1bdbc02bf44f2238b6967bd009df5ddf16
rej22 <rejilraj813@outlook.com>
2023-05-05 11:35:34 +0530

    creating a new app called detailform

--- jklbank/jklbanking/jklbanking/settings.py
+++ jklbank/jklbanking/jklbanking/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home',
     'registration',
+    'detailform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
