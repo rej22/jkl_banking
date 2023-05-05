e033444dcae3659c894d49760437fb98bdadc1d6
rej22 <rejilraj813@outlook.com>
2023-05-05 09:31:13 +0530

    creating login view

--- jklbank/jklbanking/registration/views.py
+++ jklbank/jklbanking/registration/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     return render(request, 'registration.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        userName = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username= userName, password= password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Enter valid credentials')
+            return redirect('login')
+    return render(request, 'login.html')
